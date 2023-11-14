cd339c6ac9731acdff1bee21a9f814d285db5ec2
Dhruvil <dhruvilmody98@gmail.com>
2023-11-14 13:47:01 -0500

    First Commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php" style="color:#9C6644;">Home</a>
+         <a href="about.php" style="color:#9C6644;">About</a>
+         <a href="shop.php" style="color:#9C6644;">Shop</a>
+         <a href="contact.php" style="color:#9C6644;">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="login.php" style="color:#9C6644;">Login</a>
+         <a href="register.php" style="color:#9C6644;">Register</a>
+         <a href="cart.php" style="color:#9C6644;">Cart</a>
+         <a href="orders.php" style="color:#9C6644;">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone" style="color:#9C6644;"></i> +1226-977-4873 </p>
+         <p> <i class="fas fa-phone" style="color:#9C6644;"></i> +1226-977-4873 </p>
+         <p> <i class="fas fa-envelope" style="color:#9C6644;"></i> dhruvilmody98@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt" style="color:#9C6644;"></i> Missisauga, Ontario, CA - L5V 2S7 </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f" style="color:#9C6644;"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter" style="color:#9C6644;"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram" style="color:#9C6644;"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin" style="color:#9C6644;"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> <span style="color:#9C6644;">Book Heaven</span> </p>
+
+</section>
